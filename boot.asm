cb32274ed7d56a0335bbe98e2bfeb65c0e91d246
Show <23shahardik@gmail.com>
2024-07-09 12:36:46 +0530

    Prints a string when the boots up on qemu.

--- boot.asm
+++ boot.asm
@@ -4,12 +4,27 @@ ORG 0x7c00
 BITS 16 ;using 16 bit architecture
 
 start:
-    mov ah, 0eh
-    mov al, 'A'
-    int 0x10
-
+    mov si, message ;mov addr of message label in si reg
+    call print
     jmp $
+
+
+print:
+.loop:
+    lodsb
+    cmp al,0
+    je .done
+    call print_char
+    jmp .loop
+.done:
+    ret
+
+print_char:
+    mov ah, 0eh
+    int 0x10
+    ret
+
+message: db 'Booting OS' , 0
 
 times 510-($ - $$) db 0
 dw 0xAA55 ;Boot signuature 55AA
-    
